af9d6130836a7acd4629dfc1d1bceb83430816bc
ismakach <assan23@apec.edu.kz>
2026-05-14 17:26:28 +0500

    Добавьте файлы проекта.

---
+++ SchoolApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SchoolApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SchoolApp.Pages")]
